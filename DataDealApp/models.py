5ae02713c2dbd6351b80153cf0f11aef3b4d5a2b
sorker <38181334+sorker@users.noreply.github.com>
2019-04-15 16:22:20 +0800

    配置文件

--- DataDealApp/models.py
+++ DataDealApp/models.py
@@ -4,16 +4,19 @@ from django.db import models
 
 class SiteInfo(models.Model):
     site_ip = models.CharField(u'站点', max_length=100)
-    site_urls = models.CharField(u'具体地址', max_length=200)
-
 
     def __str__(self):
         return str(self.id)
 
 
 class ComplaintRawData(models.Model):
-    title = models.CharField(u'站点', max_length=100)
-    site_urls = models.CharField(u'具体地址', max_length=200)
+    title = models.CharField(u'标题', max_length=100)
+    content = models.CharField(u'内容', max_length=2000)
+    reflecting_time = models.CharField(u'反映时间', max_length=20)
+    reply_unit = models.CharField(u'答复单位', max_length=50)
+    reply_time = models.CharField(u'答复时间', max_length=20)
+    reply_opinion = models.CharField(u'答复意见', max_length=2000)
+    datetime = models.DateTimeField(u'获取时间', auto_now=True)
 
     def __str__(self):
         return str(self.id)
