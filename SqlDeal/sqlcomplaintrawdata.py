a1c1568821de28cbec9f2bcf02b29dd7b4f7e2dd
sorker <38181334+sorker@users.noreply.github.com>
2019-04-15 16:21:39 +0800

    数据库操作

---
+++ SqlDeal/sqlcomplaintrawdata.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+"""
+ @time    : 2019/4/15 16:04
+ @desc    : complaintrawdata数据库操作
+ @Author  : Sorke
+ @Email   : sorker0129@hotmail.com
+"""
+import django
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DataDeal.settings')
+django.setup()
+
+from DataDealApp.models import ComplaintRawData
+
+
+def sql_insert_complaintrawdata(title, content, reflecting_time, reply_unit, reply_time, reply_opinion):
+    ComplaintRawData.objects.create(title=title, content=content, reflecting_time=reflecting_time, reply_unit=reply_unit
+                                    , reply_time=reply_time, reply_opinion=reply_opinion)
