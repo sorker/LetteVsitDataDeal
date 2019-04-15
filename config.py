7344f783907dde0f5793e5014589f15dfa7d2401
sorker <38181334+sorker@users.noreply.github.com>
2019-04-15 16:22:09 +0800

    配置文件

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 """
 Author : sorker
 Time: 2019/4/12 16:04 
@@ -6,11 +7,12 @@ Product: PyCharm
 """
 from selenium.webdriver.common.by import By
 
-URL = 'http://zxts.zjzwfw.gov.cn/wsdt.do?method=sunshine'
-URL_TEST = 'http://www.baidu.com'
+URL = 'http://zxts.zjzwfw.gov.cn/wsdt.do?method=sunshine'   # 网址
+# URL_TEST = 'http://www.baidu.com' # 测试地址
 
-ELEMENT_DICT = {
+ELEMENT_CLICK_ORDER = {
     By.CLASS_NAME: 'delete',
+}   # 点击网页的元素的顺序
 
-}
+ELEMENT_TITLE_CONTENT = [By.XPATH, '//table[@class="tablelist1"]/tbody/tr']  # 获取标题和时间
 
