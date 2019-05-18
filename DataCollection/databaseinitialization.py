4b4e0ac2223d19fd5931abf4f544cf731800139e
sorker <38181334+sorker@users.noreply.github.com>
2019-05-18 09:33:41 +0800

    修复

--- DataCollection/databaseinitialization.py
+++ DataCollection/databaseinitialization.py
@@ -9,16 +9,19 @@ from DataCollection.registration import registeration
 from DataCollection.rawdatadeal import all_classification_words_weight, all_department_weight, all_classification_weight
 from DataCollection.filedeal import city_area_department
 from SqlDeal.sqldeletedata import sql_delete_data
-
+from time import sleep
 
 def database_initialization():
-    # sql_delete_data()
-    # registeration.next_area()
-    # all_classification_words_weight()
+    sql_delete_data()
+    registeration.next_area()
+    all_classification_words_weight()
     all_department_weight()
     all_classification_weight()
     city_area_department()
 
 
-if __name__ in '__mian__':
-    print(database_initialization())
+if __name__ in "__main__":
+    while True:
+        database_initialization()
+        sleep(3600)
+
